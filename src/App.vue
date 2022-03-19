2896142e965b40346e2e81ab22bb453ba0b36919
iyhong <loverman85@hanmail.net>
2022-03-19 20:46:05 +0900

    리팩터링 1 - 리스트 아이템 컴포넌트 공통화  - 뉴스 리스트 스타일링

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,23 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+
+a {
+  color: #34495e;
+  text-decoration: none;
+}
+a:hover {
+  color: #42b883;
+  text-decoration: underline;
+}
+a.router-link-exact-active {
+  text-decoration: underline;
+}
 .page-enter-active,
 .page-leave-active {
   transition: opacity 0.5s;
